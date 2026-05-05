1f6c39f00707362a37fc4962744bad9126263907
Ing Simón Briceño <sabh20020509@gmail.com>
2026-05-05 14:12:29 -0400

    fix: agregar URLs de debug_toolbar condicionalmente en DEBUG

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,3 +8,7 @@ urlpatterns = [
     path('inventario/', include('apps.lab_inventory.urls', namespace='lab_inventory')),
     path('notificaciones/', include('apps.notifications.urls', namespace='notifications')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns.insert(0, path('__debug__/', include(debug_toolbar.urls)))
